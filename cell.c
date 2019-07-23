90d87548c76fb479c8fc15b0e96c3b13a4c04d59
Mauro Granado <marianarojas65@gmail.com>
2019-07-23 15:11:54 -0300

    comentarios añadidos al creador de la red

--- cell.c
+++ cell.c
@@ -1,13 +1,17 @@
 #include "cell.h"
 
 void init_cells(CellList *clist, System *sys, double size) {
-  clist->cells_side = ceil(sys->size / size);
+  clist->cells_side = ceil(sys->size / size);			// lado de la red, divido por 2.5 (es el input) y redondeado parriba  = cant de celdas x lado
   clist->size = sys->size/clist->cells_side;
-  clist->ncells = clist->cells_side * clist->cells_side * clist->cells_side;
-  clist->list = (Cell *) malloc(clist->ncells * sizeof(Cell));
+  clist->ncells = clist->cells_side * clist->cells_side * clist->cells_side;	// cantidad de celdas totales
+
+  clist->list = (Cell *) malloc(clist->ncells * sizeof(Cell));		// guarda espacio en la memoria para las n cells en la estruct Cell
+
+
+// define la red cubica
 
   int index = 0;
-  for (int i = 0; i < clist->cells_side; i++) {
+  for (int i = 0; i < clist->cells_side; i++) {			// itera sobre las 3 coordenadas de celdas
     for (int j = 0; j < clist->cells_side; j++) {
       for (int k = 0; k < clist->cells_side; k++) {
         Cell this_cell;
@@ -15,14 +19,18 @@ void init_cells(CellList *clist, System *sys, double size) {
         this_cell.iy = j;
         this_cell.iz = k;
         this_cell.nneigh = 0;
+
         this_cell.neigh = (int *) malloc(clist->ncells * sizeof(int));
         this_cell.particles = (int *) malloc(sys->n_particles * sizeof(int));
         this_cell.n_particles = 0;
-        clist->list[index] = this_cell;
+        
+	clist->list[index] = this_cell;		// etiqueta de n de celda
         index += 1;
       }
     }
   }
+
+// calcula las distancias entre partículas vecinas y deja las que están a una distancia menor a 2,5
 
   for (int i = 0; i < clist->ncells; i++) {
     for (int j = i + 1; j < clist->ncells; j++) {
@@ -52,15 +60,19 @@ void init_cells(CellList *clist, System *sys, double size) {
   }
 }
 
+// actualiza las nuevas posiciones de las partículas de la red (después de haberlas dispuesto o interactuado)
 
 void update_cells(CellList *clist, System *sys) {
   for (int i = 0; i < clist->ncells; i++)
     clist->list[i].n_particles = 0;
+  
   for (int i = 0; i < sys->n_particles; i++) {
     int ix = floor(sys->position[3*i + 0]/clist->size);
     int iy = floor(sys->position[3*i + 1]/clist->size);
     int iz = floor(sys->position[3*i + 2]/clist->size);
+ 
     int cell_index = ix * clist->cells_side * clist->cells_side + iy * clist->cells_side + iz;
+ 
     Cell *cell = &(clist->list[cell_index]);
     cell->particles[cell->n_particles] = i;
     cell->n_particles += 1;
