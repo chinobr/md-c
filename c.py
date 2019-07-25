53e8dd83081b4b3c00b81c610d2ed5feb9f8d71f
Dalmatian314 <dalma.facu@gmail.com>
2019-07-25 18:06:12 -0300

    version corregida

--- c.py
+++ c.py
@@ -9,18 +9,18 @@ import numpy as np
 
 class CondicionesIniciales(object):
 
-"""Posiciones iniciales"""
-
-      def __init__(self, particles, size):
-         self.particles = particles
-         self.size      = size
-         self.names     = None
-         self.coords    = None
-         self.vel       = None
-         self.force     = None 
- 
+#Posiciones iniciales
+      def __init__(self, particles, size, temp):
+         self.particles   = particles
+         self.size        = size
+         self.temp        = temp 
+         self.names       = None
+         self.coords      = None
+         self.vel         = None
+         self.force       = None 
+       
       def cubo(self):
-         position    = np.zeros((self.particles, 3),dtype=np.float64)       #define arreglo posición
+         position    = np.zeros((self.particles, 3))                        #define arreglo posición
          number_side = math.ceil(self.particles)**(1/3)                     #calcula raiz cubica de particulas(Lado de la red cúbica)  
          distance    = self.size/number_side                   
                                                                             #Ubica las particulas una al lado de la otra en eL arreglo cúbico
@@ -38,17 +38,15 @@ class CondicionesIniciales(object):
 
 
       def cubo_random(self):                                  #Define posiciones random de las partículas en el cubo    
-         number_side = math.ceil(self.particles)**(1/3)               
-         distance    = self.size/number_side
-         position    = np.random.random_sample((self.particles, 3))
-         random      = position * distance                            
-         self.coords = random 
+          random   =  np.random.random_sample((self.cantidad_particulas, 3))
+          position = self.size * random
+          self.coords = position
 
       def read_coords_xyz(self,nombre_archivo):
          f      = open(nombre_archivo, 'r')                  #abre el archivo 
          lines  = f.readlines()                              #lee el archivo por líneas 
          natoms = int(lines[0].strip())                      #toma el elemento 0 de la lista y lo define como el natoms
-         coords = np.zeros((natoms, 3),dtype=np.float64)     #define arreglo de coords
+         coords = np.zeros((natoms, 3))                      #define arreglo de coords
          names  = [0] * natoms                               #lista de elementos
          for i in range(natoms):
              coords[i, :] = [float(x) for x in lines[i+2].strip().split()[1:4]]   #omito las 2 primeras líneas
@@ -61,9 +59,9 @@ class CondicionesIniciales(object):
          lines    = f.readlines()                               #lee el archivo por líneas
          natoms   = int(lines[3].strip())                       #toma el elemento 3 de la lista y lo define como el natoms 
          line_def = str(lines[8].strip())                       #línea que define las variables calculadas en LAMMPS	
-         coords   = np.zeros((natoms, 3), dtype=np.float64)     #define arreglo de coords
-         vel      = np.zeros((natoms, 3), dtype=np.float64)     #define arreglo de velocidad
-         force    = np.zeros((natoms,3), dtype=np.float64)      #define arreglo de fuerza
+         coords   = np.zeros((natoms, 3))                       #define arreglo de coords
+         vel      = np.zeros((natoms, 3))                       #define arreglo de velocidad
+         force    = np.zeros((natoms,3))                        #define arreglo de fuerza
          names    = [0] * natoms
          if line_def == "ITEM: ATOMS element x y z vx vy vz fx fy fz":
             for i in range(natoms):
@@ -77,14 +75,16 @@ class CondicionesIniciales(object):
          self.vel    = vel
          self.force  = force
 
-"""Velocidades Iniciales"""                 
+#Velocidades Iniciales                 
  
       def vel_random(self):                                                          #Calcula la velocidad aleatoriamente                
          v_in     = np.random.random_sample((self.particles, 3), dtype=np.float64)   #Genera un array random
          v_cm     = v_in - np.mean(v, axis=1, keepdims = True)                       #Le resta el centro de masa a la velocidad
-         self.vel = v_cm
+         v_rn     = v_cm * np.sqrt(self.temp)
+         self.vel = v_rn
        
-      def vel_boltzman(self, T, masa):                                                                 #Calcula la velocidad con la distribución de Boltzman
-         v_in      = np.random.normal(loc = 0, scale = 1, size = (self.particles,3), dtype=np.float64) #Genera un array con distribución gaussiana
-         v_bol     = v_in * np.sqrt( T / masa)                                      
+      def vel_boltzman(self):                                                        #Calcula la velocidad con la distribución de Boltzman
+         v_in      = np.random.normal(loc = 0, scale = 1, size = (self.particles,3)) #Genera un array con distribución gaussiana
+         v_cm      = v_in - np.mean(v, axis=1, keepdims = True)
+         v_bol     = v_cm * np.sqrt(self.temp)                                       #Considerando masa=1
          self.vel  = v_bol
